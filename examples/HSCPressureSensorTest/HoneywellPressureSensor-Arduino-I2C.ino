f340dc5a6a701216d048cd75e9b1115ee4e2de3f
Lollo <54142484+LukasHirsch@users.noreply.github.com>
2020-06-17 10:01:53 +0200

    Create HoneywellPressureSensor-Arduino-I2C.ino

---
+++ examples/HSCPressureSensorTest/HoneywellPressureSensor-Arduino-I2C.ino
@@ -0,0 +1,33 @@
+/*
+ * PressureSensorTest
+ *
+ * Fetch and print values from a Honeywell 
+ * Pressure Sensor over I2C using an Arduino
+ * 
+ * The sensor values used in this demo are 
+ * for a -40 to 40 psi/mbar/bar gauge pressure sensor.
+ * The unit depends on your sensor.
+ */
+
+#include <HoneywellTruStabilityI2C.h>
+
+#define I2C_ADDRESS 0x28    // this is the default address of honeywell i2c pressure sensors
+TruStabilityPressureSensor sensor( -40.0, 40.0 );
+
+void setup() {
+  Serial.begin(115200);   // start Serial communication
+  sensor.begin();         // run sensor initialization
+}
+
+void loop() {
+  // the sensor returns 0 when new data is ready
+  if( sensor.readSensor() == 0 ) {
+    Serial.print( "temp [C]: " );
+    Serial.print( sensor.temperature() );
+    Serial.print( "\t pressure [psi/mbar/bar]: " );
+    Serial.println( sensor.pressure() );
+  }
+  
+  delay( 500 ); // Slow down sampling to 2 Hz. This is just a test.
+
+}
